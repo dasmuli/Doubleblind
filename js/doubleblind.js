8f4b161a824b49733594e86c9ecee663f0bdaf32
Dirk <dasmuli@web.de>
2020-05-17 15:17:49 +0200

    Added download button with test data.

--- js/doubleblind.js
+++ js/doubleblind.js
@@ -759,6 +759,16 @@ var UIController = {
 		  this.selectedMapY = AllUnits[this.currentEditedUnit].mapY
 		}
 	},
+	download: function(filename, text) {
+      var element = document.createElement('a');
+      element.setAttribute('href', 
+	    'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+      element.setAttribute('download', filename);
+      element.style.display = 'none';
+      document.body.appendChild(element);
+      element.click();
+      document.body.removeChild(element);
+    }
 }
 GameEngine.autoload()
 UIController.showMapNow()
