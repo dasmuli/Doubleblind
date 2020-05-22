c14285d5ae7a786b014897e1d9866328cc52b820
Dirk <dasmuli@web.de>
2020-05-22 11:18:54 +0200

    Installation on PC works.

--- js/doubleblind.js
+++ js/doubleblind.js
@@ -1027,4 +1027,51 @@ document.getElementById('fileInput').addEventListener('change', function selecte
   reader.readAsText(this.files[0]);
 });
 
+////////////////////////////////////////////////////////////
+// Installer
+// installation stuff: service worker needed
+console.log('Service worker...');
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js', {})
+  .then((reg) => {
+    // registration worked
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch((error) => {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
+else
+{
+	console.log('No service worker in app');
+}
+// catch installable event
+let deferredPrompt;
+// hide installation hint
+document.getElementById('installationHint').style.display = "none"
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  document.getElementById('installationHint').style.display = "block"
+  document.getElementById('installButton').addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  document.getElementById('installationHint').style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
+//////////////////////////////////////////////////////////////
+
 GameEngine.autoload()
